9778149b32b42a416bbc040cfd08320fae166f0a
Andrew Skvrotsov <medif625@gmail.com>
2016-07-05 23:26:10 +0300

    now server can send 404 status if specified handler not found or file not exist

--- pyserv/pyserv.py
+++ pyserv/pyserv.py
@@ -1,6 +1,8 @@
 import socket
 import re
 import os
+import sys
+from datetime import datetime
 from urllib.parse import urlparse
 from http.server import HTTPServer
 from .enchanted import StaticHandler
@@ -35,14 +37,14 @@ class App(object):
         return None
 
     def linesplit(self, socket):
-        _buffer = socket.recv(2)
+        _buffer = socket.recv(64)
         buffering = True
         while buffering:
             if b"\n" in _buffer:
                 (line, _buffer) = _buffer.split(b"\n", 1)
                 return line + b"\n"
             else:
-                more = socket.recv(2)
+                more = socket.recv(64)
                 if not more:
                     buffering = False
                 else:
@@ -53,10 +55,14 @@ class App(object):
     def __call__(self, request, client_address, _self):
         try:
             requestline = self.linesplit(request).decode()
-            path = requestline.split()[1]
+            method, path, version = requestline.split()
             handler = self.get_handler(path)
 
             if handler is None:
+                not_found_msg = "%s 404 Not Found\r\n" % version
+                request.send(not_found_msg.encode())
+                current_time = datetime.strftime(datetime.now(),"%d/%b/%Y %H:%M:%S")
+                sys.stderr.write("%s - - [%s] '%s %s %s' 404\n" % (request.getsockname()[0], current_time, method, path, version))
                 return
         except socket.timeout as e:
             return
